5af92e98eda5887a297791a4ec09b795c9116ef9
Nick Zhang <zzy@mit.edu>
2021-01-30 17:58:20 -0500

    Dongliang's original matlab implementation

---
+++ MATLAB/benchmarkRRT.m
@@ -0,0 +1,42 @@
+
+% clc;
+% close all;
+% clear all;
+
+num_of_runs = 20;
+run_RRTstar = 1;
+
+dim=3;
+ 
+if dim == 2
+    stepsize = 4;
+    radius = 3;
+    samples = 2000;
+elseif dim == 3
+    stepsize = 4;
+    radius = 3;
+    samples = 2000;
+end
+
+show_output = 1;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+    ItsM = [];
+    timeM = [];
+    costM = [];
+    segmentLength = stepsize;
+
+    if run_RRTstar == 1
+
+        for i = 1:num_of_runs
+            [Its, time, cost] = RRTstar3D(dim, segmentLength, radius, show_output, samples);
+            costM = [costM; cost];
+            timeM = [timeM; time];
+            ItsM = [ItsM; Its];
+        end
+
+    end
