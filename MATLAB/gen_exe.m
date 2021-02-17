99ddeafc186bf155e6e8ac7d362bf9e539486b2b
Nick Zhang <zzy@mit.edu>
2021-02-17 17:51:31 -0500

    fix GChild size

--- MATLAB/gen_exe.m
+++ MATLAB/gen_exe.m
@@ -1,5 +1,5 @@
-cfg = coder.config('exe')
-codegen -report -config cfg benchmarkRRT main.c main.h
+%cfg = coder.config('exe')
+%codegen -report -config cfg benchmarkRRT main.c main.h
 %system("benchmarkRRT.exe")
-%load('H:\DCSL\dongliang_rrt_star_v2\codegen\lib\benchmarkRRT\buildInfo.mat')
-%packNGo(buildInfo, 'fileName', 'benchmarkRRT.zip');
+load('H:\DCSL\dongliang_rrt_star_v2\codegen\lib\benchmarkRRT\buildInfo.mat')
+packNGo(buildInfo, 'fileName', 'benchmarkRRT.zip');
