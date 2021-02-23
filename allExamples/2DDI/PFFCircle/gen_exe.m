6a6bbaea1b5b33a8c299c98a551b56e1747efb83
Nick Zhang <zzy@mit.edu>
2021-02-22 22:03:26 -0500

    update script to gen zip

--- allExamples/2DDI/PFFCircle/gen_exe.m
+++ allExamples/2DDI/PFFCircle/gen_exe.m
@@ -1,5 +1,8 @@
+%% generate exe
 cfg = coder.config('exe')
 codegen -report -config cfg benchmarkRRT main.c main.h
 system("benchmarkRRT.exe")
-% load('H:\DCSL\dongliang_rrt_star_v2\codegen\lib\benchmarkRRT\buildInfo.mat')
-% packNGo(buildInfo, 'fileName', 'benchmarkRRT.zip');
+
+%% package
+load('H:\DCSL\dongliang_rrt_star_v2\codegen\lib\benchmarkRRT\buildInfo.mat')
+packNGo(buildInfo, 'fileName', 'D:\Documents\GitHub\kinoRRTstar\allExamples\2DDI\PFFCircle\benchmarkRRT.zip');
